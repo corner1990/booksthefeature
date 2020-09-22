6d00389b24bd179f817c527535435ef3a22537d3
corner <804160646@qq.com>
2020-09-23 01:25:36 +0800

    完成订单详情页面

--- src/pages/order-detail/components/oder-info.jsx
+++ src/pages/order-detail/components/oder-info.jsx
@@ -6,19 +6,23 @@ import './index.scss'
  * @desc 详情头部
  */
 const OrderInfo = () => {
-  return (<View className='OrderInfoWrap'>
-    <View className='Title'>收货信息</View>
+  return (<View className='OrderInfoWrap ReceiptInfoWrap'>
+    <View className='Title'>订单信息</View>
     <View className='Line'>
-      <Text className='LineTitle'>收货人：</Text>
-      <Text className='LineText'>Vian</Text>
+      <Text className='LineTitle'>订单号码：</Text>
+      <Text className='LineText'>42343243242324</Text>
     </View>
     <View className='Line'>
-      <Text className='LineTitle'>手机号码：</Text>
-      <Text className='LineText'>18662478953</Text>
+      <Text className='LineTitle'>下单时间：</Text>
+      <Text className='LineText'>2019.09.17 13:00:00</Text>
     </View>
-    <View className='Line AddrLine'>
-      <Text className='LineTitle'>收货地址：</Text>
-      <Text className='LineText'>上海市 静安区 陕西北路 100弄 1号</Text>
+    <View className='Line'>
+      <Text className='LineTitle'>支付方式：</Text>
+      <Text className='LineText'>微信支付</Text>
+      </View>
+    <View className='Line'>
+      <Text className='LineTitle'>祝福卡：</Text>
+      <Text className='LineText'>微信</Text>
     </View>
   </View>)
 }
